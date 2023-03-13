4b9ca9e9b08fffec83fbc28063f88aac58112aa5
Jordan Welch <jordan.welch@complyant.co>
2023-03-13 14:33:02 -0500

    Add full explainer

--- resources/views/livewire/thing-to-type-form.blade.php
+++ resources/views/livewire/thing-to-type-form.blade.php
@@ -1,7 +1,17 @@
-<form wire:submit.prevent="submit">
-    {{ $this->form }}
+<div class="max-w-lg p-10">
+    <div class="prose">
+        <h1 class="text-lg">To recreate bug</h1>
+        <ol>
+            <li>Pick any <strong>Type</strong> from first select.</li>
+            <li>Click <strong>[Tab]</strong> key until <strong>Thing</strong> select is active</li>
+            <li>Type a single character</li>
+            <li>See that search is not filtering list.</li>
+        </ol>
 
-    <button type="submit">
-        Submit
-    </button>
-</form>
+    </div>
+
+
+    <form wire:submit.prevent="submit">
+        {{ $this->form }}
+    </form>
+</div>
