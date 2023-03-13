5fa154b44ffd50c54fc7a4d6687e237758007a27
Jordan Welch <jordan.welch@complyant.co>
2023-03-13 15:38:23 -0500

    Add sorts

--- app/Http/Livewire/ThingToTypeForm.php
+++ app/Http/Livewire/ThingToTypeForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Thing;
+use App\Models\Type;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms;
@@ -19,15 +21,16 @@ class ThingToTypeForm extends Component implements HasForms
         return [
             Forms\Components\Select::make('type_id')
                 ->options(function () {
-                    return \App\Models\Type::all()->pluck('name', 'id');
+                    return Type::orderBy('name')
+                        ->pluck('name', 'id');
                 })
                 ->label('Type')
                 ->required(),
 
             Forms\Components\Select::make('thing_id')
                 ->options(function ($get) {
-                    return \App\Models\Thing::all()
-                        ->where('type_id', $get('type_id'))
+                    return Thing::where('type_id', $get('type_id'))
+                        ->orderBy('name')
                         ->pluck('name', 'id');
                 })
                 ->searchable()
